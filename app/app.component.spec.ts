ba94cab882d8496a4a7c64dc7a3b69a67de7e258
Jagadeesan <137260927+jagadeesan025@users.noreply.github.com>
2023-07-17 16:20:07 +0530

    ecom

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'democopy'`, () => {
+  it(`should have as title 'ecommers'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('democopy');
+    expect(app.title).toEqual('ecommers');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('democopy app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ecommers app is running!');
   });
 });
