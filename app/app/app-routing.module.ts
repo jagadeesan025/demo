045befb60a64f895c1079912c54cd407abfd1f03
Jagadeesan <137260927+jagadeesan025@users.noreply.github.com>
2023-07-17 16:28:14 +0530

    Add files via upload

--- app/app/app-routing.module.ts
+++ app/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { DemoComponent } from './demo/demo.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  {path:'home', component:HomeComponent},
-  {path:'about', component:AboutComponent}
+const routes: Routes = [ {
+  path:'main', component:HomeComponent
+},
+{path:'demo', component:DemoComponent}
+
 ];
 
 @NgModule({
--- app/app/app.component.spec.ts
+++ app/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GTSA'`, () => {
+  it(`should have as title 'skechers'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GTSA');
+    expect(app.title).toEqual('skechers');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GTSA app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('skechers app is running!');
   });
 });
